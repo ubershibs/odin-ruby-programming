954963fdbc30950041428fd3934f13ab48193d8a
ubershibs <luke..walker@gmail.com>
2015-11-26 13:12:03 -0500

    Reworked stock picker to deal with max profit range than simply max/min.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,22 @@
 def stock_picker(prices)
-	low = prices.index(prices[0..-2].min)
-	high = prices.index(prices[low..-1].max)
-	return [low, high]
+
+buy_day = 0
+sell_day = 0
+max_profit = 0
+
+
+prices.each_with_index do |buy_price, i|
+	prices[i+1..-1].each_with_index do |sell_price, j|
+		if (sell_price - buy_price > max_profit)
+			max_profit = sell_price - buy_price
+			buy_day = i
+			sell_day = j+i+1
+		end
+	end
 end
 
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+return [buy_day, sell_day]
+
+end
+
+puts stock_picker([17,3,18,6,9,15,8,6,3,18,10,0])
